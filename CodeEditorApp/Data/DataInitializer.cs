5bd532970c46a9ba4bc01783f149698326a1b2ba
vilhjalmur12 <vilhjalmur12@ru.is>
2017-04-28 13:22:37 +0000

    byrjað á gagnagrunnstengingu

---
+++ CodeEditorApp/Data/DataInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeEditorApp.Data
+{
+    public class DataInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<DataContext> 
+    {
+        protected override void seed (DataContext context)
+        {
+
+        }
+
+    }
+}
