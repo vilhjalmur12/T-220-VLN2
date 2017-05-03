110d399dff675b0f1a89ee86b9a89d260b34fbf2
vilhjalmur12 <vilhjalmur12@ru.is>
2017-05-03 07:43:37 +0000

    view models og service

---
+++ CodeEditorApp/Models/ViewModels/CommentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeEditorApp.Models.ViewModels
+{
+    public class CommentViewModel
+    {
+        public string Content { get; set; }
+        public 
+    }
+}
---
+++ CodeEditorApp/Models/ViewModels/GoalViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeEditorApp.Models.ViewModels
+{
+    public class GoalViewModel
+    {
+    }
+}
