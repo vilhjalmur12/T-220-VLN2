02fb02ad1b2884430efbbcda8da0a3b4366e9628
Berglind <Berglind@DESKTOP-5VJOV7E>
2017-05-04 09:09:55 +0000

    skyringar

--- CodeEditorApp/Models/Entities/File.cs
+++ CodeEditorApp/Models/Entities/File.cs
@@ -6,11 +6,29 @@ using System.Web;
 
 namespace CodeEditorApp.Models
 {
+    /// <summary>
+    /// A File represents a file that is in a Project.
+    /// Project may contain multiple files.
+    /// Files can, for example, contained html code or c++ code.
+    /// </summary>
     public class File
     {
+        /// <summary>
+        /// The database generates unique ID of the File.
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// The name of the file, Example:CommentViewModel.cs
+        /// </summary>
         public string name { get; set; }
+        /// <summary>
+        /// The location of the file.
+        /// Example: username/folder1/project1/File.cs
+        /// </summary>
         public string location { get; set; }
+        /// <summary>
+        /// The ID of the Project that the File belongs to. 
+        /// </summary>
         public int ProjectID { get; set; }
      //   public virtual Project project { get; set; }
     }
