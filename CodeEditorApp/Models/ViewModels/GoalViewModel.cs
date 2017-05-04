f6f791a400d369b57e8de91265e713af24911fd5
vilhjalmur12 <vilhjalmur12@ru.is>
2017-05-04 16:24:28 +0000

    viewModelClasses

--- CodeEditorApp/Models/ViewModels/GoalViewModel.cs
+++ CodeEditorApp/Models/ViewModels/GoalViewModel.cs
@@ -7,5 +7,12 @@ namespace CodeEditorApp.Models.ViewModels
 {
     public class GoalViewModel
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public bool Checked { get; set; }
+        public GoalType Type { get; set; }
+        public AspNetUser User { get; set; }
+        public int ProjectID { get; set; }
     }
 }
