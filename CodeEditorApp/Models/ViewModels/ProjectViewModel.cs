b0432c94e21e2e398e6e17a8dce1c7ac3c1c7f4b
Berglind <Berglind@DESKTOP-5VJOV7E>
2017-05-04 08:42:18 +0000

    UserHomeRepository

--- CodeEditorApp/Models/ViewModels/ProjectViewModel.cs
+++ CodeEditorApp/Models/ViewModels/ProjectViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace CodeEditorApp.ViewModels
+namespace CodeEditorApp.Models.ViewModels
 {
     public class ProjectViewModel
     {
