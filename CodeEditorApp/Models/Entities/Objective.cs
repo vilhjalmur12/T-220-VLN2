8835f010b5a17dc830a79bf04d78e41c90a7dbc9
Berglind <Berglind@DESKTOP-5VJOV7E>
2017-05-05 12:55:05 +0000

    projectrepo

--- CodeEditorApp/Models/Entities/Objective.cs
+++ CodeEditorApp/Models/Entities/Objective.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace CodeEditorApp.Models.Entities
+namespace CodeEditorApp.Models
 {
     public class Objective
     {  
