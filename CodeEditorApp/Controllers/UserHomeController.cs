454f9f565dfb7f2862cdeaa5e5473dda497b20ae
Berglind Sigurðardóttir <berglinds14@ru.is>
2017-05-09 13:19:47 +0000

    project

--- CodeEditorApp/Controllers/UserHomeController.cs
+++ CodeEditorApp/Controllers/UserHomeController.cs
@@ -68,11 +68,11 @@ namespace CodeEditorApp.Controllers
         {
             if (projectID.HasValue)
             {
-                ProjectViewModel model = UserHome.GetProjectByID(projectID.Value);
-
-                return RedirectToAction("Index", "Project", model);
+                TempData["projectModel"] = UserHome.GetProjectByID(projectID.Value);
+                return RedirectToAction("Index", "Project");
             }
-            return RedirectToAction("Index", "Project");
+
+            return RedirectToAction("Index", "UserHome");
         }
 
         public ActionResult OpenProjectByFile(int fileID)
