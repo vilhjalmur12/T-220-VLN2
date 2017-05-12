0a76fe144487cba3f00d85009bd111442a838950
Iris <Iris@DESKTOP-UQGV1HO>
2017-05-12 10:47:26 -0700

    ..

--- CodeEditorApp/Repositories/ProjectRepository.cs
+++ CodeEditorApp/Repositories/ProjectRepository.cs
@@ -26,17 +26,22 @@ namespace CodeEditorApp.Repositories
 
         public FileType GetFileTypeByExtension(string Ext)
         {
-
-
             return _db.FileTypes.Where(x => x.Extension == Ext).SingleOrDefault();
         }
-
+        /// <summary>
+        ///Creates a file 
+        /// </summary>
+        /// <param name="file"></param>
         public void CreateFile(ref File file)
         {
             _db.Files.Add(file);
             _db.SaveChanges();
         }
-
+        /// <summary>
+        /// Get the view model from the project open
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>returns the project open</returns>
         public OpenProjectViewModel GetOpenProjectViewModel(int projectID)
         {
             Project project = _db.Projects.Find(projectID);
@@ -52,6 +57,11 @@ namespace CodeEditorApp.Repositories
             return newOpenProjectModel;
         }
 
+        /// <summary>
+        /// Gets the solution folder desired by ID
+        /// </summary>
+        /// <param name="solutionFolderID"></param>
+        /// <returns></returns>
         public FolderViewModel GetSolutionFolder(int solutionFolderID)
         {
             Folder solutionFolder = _db.Folders.Find(solutionFolderID);
@@ -68,6 +78,11 @@ namespace CodeEditorApp.Repositories
             return newSolutionFolderViewModel;
         }
 
+        /// <summary>
+        /// Gets the goals listed in the project by project id
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>List of the goals in the project</returns>
         public List<GoalViewModel> GetGoalsByProject(int projectID)
         {
             List<GoalViewModel> goalModels = new List<GoalViewModel>();
@@ -90,7 +105,11 @@ namespace CodeEditorApp.Repositories
             return goalModels;
         }
 
-        //Hér sækjum við lista af notendum eftir projectID
+        /// <summary>
+        /// Here we get a list of members in a project 
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>The list of the members in wanted project ID</returns>
         public List<UserViewModel> GetUsersByProject(int projectID)
         {
             List<UserViewModel> userModels = new List<UserViewModel>();
@@ -123,9 +142,13 @@ namespace CodeEditorApp.Repositories
             return ReturnUser;
         }
 
+        /// <summary>
+        /// Get files from a desired projectID 
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>Returns a list of files in the projectID desired</returns>
         public List<FileViewModel> GetFilesByProject(int projectID)
         {
-            // TODO
             List<FileViewModel> fileModels = new List<FileViewModel>();
             _db.Files.ToList().ForEach(file =>
             {
@@ -143,6 +166,11 @@ namespace CodeEditorApp.Repositories
             return fileModels;
         }
 
+        /// <summary>
+        /// Get file with desired ID
+        /// </summary>
+        /// <param name="FileID"></param>
+        /// <returns>Returns the file wanted</returns>
         public FileViewModel GetFileByID (int FileID)
         {
             File file = _db.Files.Where(x => x.ID == FileID).SingleOrDefault();
@@ -160,9 +188,13 @@ namespace CodeEditorApp.Repositories
             return ReturnFile;
         }
 
+        /// <summary>
+        /// Get folders in the project you are in
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>Returns a list of a folders in project</returns>
         public List<FolderViewModel> GetFoldersByProject (int projectID)
         {
-            //TODO
             List<FolderViewModel> folderModels = new List<FolderViewModel>();
             _db.Folders.ToList().ForEach(folder =>
             {
@@ -182,6 +214,10 @@ namespace CodeEditorApp.Repositories
             return folderModels;
         }
 
+        /// <summary>
+        /// Adds a new goal to project
+        /// </summary>
+        /// <param name="goalModel"></param>
         public void AddGoal(GoalViewModel goalModel)
         {
             Goal newGoal = new Goal()
@@ -197,6 +233,10 @@ namespace CodeEditorApp.Repositories
             _db.SaveChanges();
         }
 
+        /// <summary>
+        /// Removes a goal from a project
+        /// </summary>
+        /// <param name="goalModel"></param>
         public void RemoveGoal(GoalViewModel goalModel)
         {
             Goal theGoal = _db.Goals.Find(goalModel.ID);
@@ -208,6 +248,10 @@ namespace CodeEditorApp.Repositories
             }
         }
 
+        /// <summary>
+        /// This function adds user to a project
+        /// </summary>
+        /// <param name="membershipModel"></param>
         public void AddUserToProject(MembershipViewModel membershipModel)
         {
             Membership newMembership = new Membership()
@@ -219,7 +263,10 @@ namespace CodeEditorApp.Repositories
             _db.SaveChanges();
         }
 
-        //Removes user from project
+        /// <summary>
+        /// This function removes user from a project if
+        /// </summary>
+        /// <param name="membership"></param>
         public void RemoveMemberFromProject (MembershipViewModel membership)
         {
 
@@ -234,9 +281,12 @@ namespace CodeEditorApp.Repositories
             }
         }
 
+        /// <summary>
+        /// Removes a file
+        /// </summary>
+        /// <param name="fileID"></param>
         public void RemoveFile(int fileID)
         {
-            //TODO
             File RFile = _db.Files.Where(x => x.ID == fileID).SingleOrDefault();
             if (RFile != null)
             {
@@ -245,39 +295,10 @@ namespace CodeEditorApp.Repositories
             }
         }
 
-        public IEnumerable<FileViewModel> ListFilesByProject(int projectID)
-        {
-            //TODO
-            return null;
-        }
-
-        public void AddFileToProject(int projectID)
-        {
-            //TODO
-        }
-
-        public void EditProject(int projectID)
-        {
-            //TODO
-        }
-
-        public void EditFile(int fileID)
-        {
-            //TODO
-        }
-
-        public void EditFolder(int folderID)
-        {
-            //TODO
-        }
-
-        public void AddFileToFolder(int folderID)
-        {
-            //TODO
-        }
-
-
-        // Adds user to project if user exists. Returns true if user was added to project, else returns false.
+        /// <summary>
+        /// Adds member to a project if user is a registered user and is not a member already
+        /// </summary>
+        /// <param name="membershipModel"></param>
         public void AddMemberIfExists(MembershipViewModel membershipModel)
         {
             ApplicationUser user = FindUserByEmail(membershipModel.Email);
@@ -292,12 +313,21 @@ namespace CodeEditorApp.Repositories
                 }
             }
         }
-
+        /// <summary>
+        /// Finds project in the database by the ID of the project
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns>Returns the </returns>
         private Project FindProjectByID(int projectID)
         {
             return _db.Projects.Find(projectID);
         }
 
+        /// <summary>
+        /// Finds if user is already a member in a project
+        /// </summary>
+        /// <param name="membershipModel"></param>
+        /// <returns>returns true if user is a member and false if not</returns>
         private bool MembershipExists(MembershipViewModel membershipModel)
         {
             foreach (Membership membership in _db.Memberships.ToList())
@@ -310,27 +340,23 @@ namespace CodeEditorApp.Repositories
             return false;
         }
 
+        /// <summary>
+        /// Finds user by email address
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>returns the user with desired email</returns>
         private ApplicationUser FindUserByEmail(string email)
         {
             ApplicationUser myUser = _db.Users.SingleOrDefault(user => user.Email == email);
             return myUser;
         }
 
-        /*public bool RemoveMemberIfInProject(string email, int projectID)
-        {
-            List<UserViewModel> userList = GetUsersByProject(projectID); //sæki lista af notendum sem eru í þessu projectID-i
 
-            foreach (UserViewModel user in userList)
-            {
-                if (user.UserName == email)
-                {
-                    RemoveUserFromProject(user.ID, projectID); //eyðum notanda úr verkefni - veit ekki hvort að ég megi nota þetta userid
-                }
-                return true;
-            }
-            return false;
-        }*/
-
+        /// <summary>
+        /// Saves the content in a file
+        /// </summary>
+        /// <param name="fileID"></param>
+        /// <param name="content"></param>
         public void SaveFileContent (int fileID, string content)
         {
             if (content == null)
--- CodeEditorAppTest/UnitTest1.cs
+++ CodeEditorAppTest/UnitTest1.cs
@@ -3,12 +3,5 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeEditorAppTest
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+
 }
